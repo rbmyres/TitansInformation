59654d462282a8e56150510fa37e8a756fb5db68
rbmyres <rmyres3@gmail.com>
2025-01-23 13:32:23 -0600

    Updated config URL

--- project7/vite.config.js
+++ project7/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/~rbmyres/CS354/TitansInformation/project7',
+  base: '/~rbmyres/Projects/TitansInformation/project7',
   server: {
     historyApiFallback: true
   }
